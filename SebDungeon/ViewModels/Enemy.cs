5fe2b3fe7a00c2d3f89c62370b6be20a12dbc3b7
sebastian <sebastian.j.oliver@outlook.com>
2017-06-05 10:45:05 +0800

    NCG

---
+++ SebDungeon/ViewModels/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+
+namespace SebDungeon
+{
+    public class Enemy : PropertyChangedBase
+    {
+        public string Name { get; set; }
+        public int HitPoints { get; set; }
+        public bool IsAlive { get { return HitPoints > 0; } }
+        public bool HasFought { get; set; } = true;
+        private static Random _rand = new Random();
+
+        public string GetDescription()
+        {
+            var list = new List<string>();
+            if (IsAlive)
+                list.Add(string.Format("{0} has {1} hit points remaining", Name, HitPoints));
+            else
+                list.Add(string.Format("The {0} is dead", Name));
+            return string.Join("\r\n", list);
+        }
+
+        public string Fight(Hero hero)
+        {
+            if (hero == null) return null;
+            var list = new List<string>();
+            if (_rand.Next(5) <= 1)
+            {
+                var damage = _rand.Next(3) + 1;
+                list.Add(string.Format("you get hit for {0} points of damage", damage));
+                hero.HitPoints -= damage;
+                if (hero.HitPoints <= 0)
+                    list.Add(string.Format("you are KILLED by the {0}!", Name));
+            }
+            else
+            {
+                list.Add("the enemy misses!");
+            }
+            HasFought = true;
+            return string.Join("\r\n", list);
+        }
+
+    }
+
+}
---
+++ SebDungeon/ViewModels/Hero.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+
+namespace SebDungeon
+{
+    public class Hero : PropertyChangedBase
+    {
+        public int GoldCount { get; set; } = 0;
+        public int HitPoints { get; set; } = 15 + _rand.Next(10);
+        public string Name { get; set; }
+        public bool IsAlive { get { return HitPoints > 0; } }
+
+        private static Random _rand = new Random();
+
+        public string GetDescription()
+        {
+            var list = new List<String>();
+            if (GoldCount > 0)
+                list.Add(string.Format("You have {0} gold pieces and {1} hit points", GoldCount, HitPoints));
+            return string.Join("\r\n", list);
+        }
+
+        public string Fight(Enemy enemy)
+        {
+            if (enemy == null) return null;
+            var list = new List<string>();
+            if (_rand.Next(3) <= 1)
+            {
+                var damage = _rand.Next(3) + 1;
+                list.Add(string.Format("you hit the enemy for {0} points of damage", damage));
+                enemy.HitPoints -= damage;
+                if (enemy.HitPoints <= 0)
+                    list.Add(string.Format("the enemy is killed"));
+            }
+            else
+            {
+                list.Add("your attack misses!");
+            }
+            enemy.HasFought = false;
+            return string.Join("\r\n", list);
+        }
+    }
+
+}
